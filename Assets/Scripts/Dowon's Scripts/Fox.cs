bfbe83cc55971d063a8393d53f681447b360c7c8
dwl <dwlee0802@gmail.com>
2026-01-31 13:08:50 -0800

    WASD and SPACE player inputs

--- Assets/Scripts/Dowon's Scripts/Fox.cs
+++ Assets/Scripts/Dowon's Scripts/Fox.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Fox : MonoBehaviour
 {
+    public static bool Masked = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/Dowon's Scripts/Mask.cs
+++ Assets/Scripts/Dowon's Scripts/Mask.cs
@@ -1,16 +1,68 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Mask : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Mask Settings")]
+    public float maskDuration = 5f;
+
+    [SerializeField]
+    private int featherCount = 0;
+    private Coroutine maskRoutine;
+
+    public void OnUseMask()
     {
-        
+        if (Fox.Masked)
+        {
+            ExitMaskedState();
+        }
+        else if (featherCount > 0)
+        {
+            EnterMaskedState();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Feather"))
+        {
+            featherCount++;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void EnterMaskedState()
+    {
+        featherCount--;
+        Fox.Masked = true;
+
+        if (maskRoutine != null)
+            StopCoroutine(maskRoutine);
+
+        maskRoutine = StartCoroutine(MaskTimer());
+
+        Debug.Log("++Entered masked state++");
+    }
+
+    private void ExitMaskedState()
+    {
+        if (maskRoutine != null)
+        {
+            StopCoroutine(maskRoutine);
+            maskRoutine = null;
+        }
+
+        Fox.Masked = false;
+
+        Debug.Log("--Exited masked state--");
+    }
+
+    private IEnumerator MaskTimer()
+    {
+        yield return new WaitForSeconds(maskDuration);
+        Fox.Masked = false;
+        maskRoutine = null;
     }
 }
