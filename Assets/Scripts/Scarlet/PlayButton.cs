bc09e4e2c59b44d7c96994a384e52bb875c5e18f
Doodledu22 <yueqiand@usc.edu>
2026-01-31 22:56:35 -0800

    fix chicken animator

--- Assets/Scripts/Scarlet/PlayButton.cs
+++ Assets/Scripts/Scarlet/PlayButton.cs
@@ -4,13 +4,17 @@ using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
+
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(LoadFinalGame);
+        bool isMouseDisabled = Cursor.lockState == CursorLockMode.Locked || !Cursor.visible;
+        Debug.Log(isMouseDisabled);
     }
 
     void LoadFinalGame()
     {
         SceneManager.LoadScene("FinalGame");
+        Debug.Log("Started");
     }
 }
